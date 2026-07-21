567fb6317d058781fc82be573713d59de3bad772
Sohan Moyra <moyrapcsohan@gmail.com>
2026-07-21 21:31:31 +0530

    Time: 0 ms (100%), Space: 42.5 MB (67.35%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        String[] days = {"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        
+        if (month == 1 || month == 2) {
+            month += 12;
+            year -= 1;
+        }
+
+        int h = (day + (13 * (month + 1)) / 5 + year + (year / 4) - (year / 100) + (year / 400)) % 7;
+
+        return days[h];
+    }
+}
