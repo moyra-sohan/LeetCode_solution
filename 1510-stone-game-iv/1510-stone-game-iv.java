9830b127de7129020f72b6e26ae1866629f4f922
Sohan Moyra <moyrapcsohan@gmail.com>
2026-08-10 07:05:59 +0530

    Time: 107 ms (13.32%), Space: 55.4 MB (9.4%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,35 @@
+class Solution {
+    // 0 = Not calculated
+    // 1 = Alice
+    // 2 = Bob
+    int[][] memo;
+    public boolean winnerSquareGame(int n) {
+        memo = new int[n+1][2];
+        return game(n, true);
+    }
+
+    public boolean game(int n, boolean isAlice) {
+        int isAliceIdx = isAlice ? 1 : 0;
+        if(memo[n][isAliceIdx] != 0) return memo[n][isAliceIdx] == 1;
+        int c = 1;
+        boolean best = !isAlice;
+        int i = 1;
+        while(c <= n) {
+            if(!isAlice) {
+                if(!game(n-c,true)) {
+                    best = false;
+                    break;
+                }
+            } else {
+                if(game(n-c,false)) {
+                    best = true;
+                    break;
+                }
+            }
+            i++;
+            c = i*i;
+        }
+        memo[n][isAliceIdx] = best ? 1 : 2;
+        return best;
+    }
+}
